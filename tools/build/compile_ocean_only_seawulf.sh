c8ab6cdefe3c88276b90c5f1b270e06e983f715e
Christopher Pitt Wolfe <christopher.wolfe@stonybrook.edu>
2022-01-27 15:27:41 -0500

    Build script for seawulf.

--- tools/build/compile_ocean_only_seawulf.sh
+++ tools/build/compile_ocean_only_seawulf.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-mkdir -p build/gnu/ocean_only/repro/
-(cd build/gnu/ocean_only/repro/; 
-rm -f path_names; \
-../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/{config_src/infra/FMS1,config_src/memory/dynamic_symmetric,config_src/drivers/solo_driver,config_src/external,src/{*,*/*}}/ ; \
-../../../../src/mkmf/bin/mkmf -t ../../../../macOS-gnu10-openmpi.mk -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' path_names)
-(cd build/intel/ocean_only/repro/; make NETCDF=4 REPRO=1 MOM6 -j)
+# uncomment if we want reproduction
+#REPRO="REPRO=1"
+
+if [ ! -z "$REPRO" ]
+then
+  DIR="repro"
+else
+  DIR="opt"
+fi
+
+mkdir -p ocean_only/$DIR
+cd ocean_only/$DIR
+
+if [[ -e Makefile ]] ; then
+  make clean
+  rm Makefile
+fi
+
+rm -f path_names
+../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/{config_src/infra/FMS2,config_src/memory/dynamic_symmetric,config_src/drivers/solo_driver,config_src/external,src/{*,*/*}}/
+../../../../src/mkmf/bin/mkmf -t ../../seawulf-intel.mk -o "-I../../shared/$DIR" -p MOM6 -l "-L../../shared/$DIR -lfms" path_names
+make NETCDF=4 $REPRO MOM6 -j
