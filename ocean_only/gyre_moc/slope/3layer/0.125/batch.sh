0834930b0b5ffa43aae4eb64f2faaf819118386d
Christopher L. Pitt Wolfe <christopher.l.p.wolfe@gmail.com>
2022-02-24 15:33:13 -0500

    Adding 0.125 degree experiments.

---
+++ ocean_only/gyre_moc/slope/3layer/0.125/batch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=slope_0.125
+#SBATCH --output=output.txt
+#SBATCH --ntasks-per-node=28
+#SBATCH --nodes=4
+#SBATCH --time=4:00:00
+#SBATCH -p short-28core
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=christopher.wolfe@stonybrook.edu
+
+module load intel/compiler/64/2020/20.0.2
+module load intel/mkl/64/2020/20.0.2
+module load intel/mpi/64/2020/20.0.2
+module load netcdf-fortran
+
+cd $SLURM_SUBMIT_DIR
+mkdir -p OUTPUT
+mkdir -p RESTART
+
+date
+mpirun ./MOM6
+date
+
