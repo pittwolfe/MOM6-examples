2332eaf7752af37e168dba99d01faf72213d5035
Christopher L. Pitt Wolfe <christopher.l.p.wolfe@gmail.com>
2022-03-02 16:58:41 -0500

    Added hooks in MOM_boundary_update and MOM_initialization.

--- exp/jet/code/jet_initialization.F90
+++ exp/jet/code/jet_initialization.F90
@@ -21,26 +21,96 @@ implicit none ; private
 
 #include <MOM_memory.h>
 
+
+character(len=40) :: mdl = "jet_initialization" !< This module's name.
+
+! The following routines are visible to the outside world
+public jet_initialize_topography
 public jet_set_OBC_data
+public register_jet_OBC, jet_OBC_end
+
+!> Control structure for jet open boundaries.
+type, public :: jet_OBC_CS ; private
+  real :: delta = 0.3       !< asymmetry parameter
+  real :: Ro = 0.6          !< Rossby nubmer
+  real :: F = 1.0989        !< Inverse Burger number
+  real :: L = 31.0          !< Length scale [L ~> km]
+  real :: Ls, Ln            !< Jet decay scales to the south and north [L -> km]
+  real :: U0                !< Peak velocity of jet
+  real :: F_0               !< Coriolis parameter [T-1 ~> s-1]
+end type jet_OBC_CS
 
 ! A note on unit descriptions in comments: MOM6 uses units that can be rescaled for dimensional
 ! consistency testing. These are noted in comments with units like Z, H, L, and T, along with
 ! their mks counterparts with notation like "a velocity [Z T-1 ~> m s-1]".  If the units
 ! vary with the Boussinesq approximation, the Boussinesq variant is given first.
 
-!> A module variable that should not be used.
-!! \todo Move this module variable into a control structure.
-logical :: first_call = .true.
-
 contains
+
+!> Add jet to OBC registry.
+function register_jet_OBC(param_file, CS, US, OBC_Reg)
+  type(param_file_type),    intent(in) :: param_file !< parameter file.
+  type(jet_OBC_CS),         pointer    :: CS         !< jet control structure.
+  type(unit_scale_type),    intent(in) :: US         !< A dimensional unit scaling type
+  type(OBC_registry_type),  pointer    :: OBC_Reg    !< OBC registry.
+  logical                              :: register_jet_OBC
+  ! Local variables
+  real    :: km_to_L_scale  ! A scaling factor from longitudes in km to L [L km-1 ~> 1e3]
+
+  character(len=32)  :: casename = "jet"       !< This case's name.
+
+  if (associated(CS)) then
+    call MOM_error(WARNING, "register_jet_OBC called with an "// &
+                            "associated control structure.")
+    return
+  endif
+  allocate(CS)
+
+  ! Register the tracer for horizontal advection & diffusion.
+  call register_OBC(casename, param_file, OBC_Reg)
+  call get_param(param_file, mdl, "F_0", CS%F_0, &
+                 default=0.0, units="s-1", scale=US%T_to_s, do_not_log=.true.)
+  call get_param(param_file, mdl,"JET_L", CS%L, &
+                 "Jet length scale.",&
+                 units="km", default=31.0, scale=1.0e3*US%m_to_L)
+  call get_param(param_file, mdl, "JET_DELTA", CS%delta, &
+                 "Jet asymmetry parameter.",  &
+                 units="nondim", default=0.3)
+  call get_param(param_file, mdl, "JET_ROSSBY", CS%Ro, &
+                 "Jet Rossby number.",  &
+                 units="nondim", default=0.6)
+  call get_param(param_file, mdl, "JET_F", CS%F, &
+                 "Jet inverse Burger number.",  &
+                 units="nondim", default=1.0989)
+                 
+  km_to_L_scale = 1000.0*US%m_to_L
+  ! Unravel the dimensionless parameters into dimensional parameters
+  CS%Ls = CS%L*(1.0 + CS%delta)
+  CS%Ln = CS%L*(1.0 - CS%delta)
+  CS%U0 = CS%Ro*CS%F_0*CS%L*km_to_L_scale
+                 
+  register_jet_OBC = .true.
+
+end function register_jet_OBC
+
+!> Clean up the jet OBC from registry.
+subroutine jet_OBC_end(CS)
+  type(jet_OBC_CS), pointer    :: CS         !< jet control structure.
+
+  if (associated(CS)) then
+    deallocate(CS)
+  endif
+end subroutine jet_OBC_end
+
 !> This subroutine sets the properties of flow at open boundary conditions.
-subroutine jet_set_OBC_data(OBC, G, GV, US, PF)
+subroutine jet_set_OBC_data(OBC, CS, G, GV, US, PF)
   type(ocean_OBC_type),       pointer    :: OBC   !< This open boundary condition type specifies
                                                   !! whether, where, and what open boundary
                                                   !! conditions are used.
+  type(jet_OBC_CS),           pointer    :: CS    !< tidal bay control structure.
   type(ocean_grid_type),      intent(in) :: G     !< The ocean's grid structure.
   type(verticalGrid_type),    intent(in) :: GV    !< The ocean's vertical grid structure.
-  type(unit_scale_type),      intent(in) :: US  !< A dimensional unit scaling type
+  type(unit_scale_type),      intent(in) :: US    !< A dimensional unit scaling type
   type(param_file_type),      intent(in) :: param_file !< A structure indicating the
                                                   !! open file to parse for model
                                                   !! parameter values.
@@ -48,12 +118,15 @@ subroutine jet_set_OBC_data(OBC, G, GV, US, PF)
   ! local variables to set up the inflow
   integer :: i, j, k, n, is, ie, js, je, isd, ied, jsd, jed, nz
   integer :: IsdB, IedB, JsdB, JedB
+  integer :: n
+  type(OBC_segment_type), pointer :: segment => NULL()
   real    :: km_to_L_scale  ! A scaling factor from longitudes in km to L [L km-1 ~> 1e3]
   real    :: delta                                  !! asymmetry parameter
   real    :: F                                      !! Inverse Burger number
   real    :: Ro                                     !! Rossby number
   real    :: L                                      !! Length scale [L ~> km]
   real    :: Ls, Ln                                 !! Jet decay scales to the south and north [L -> km]
+  real    :: U0                                     !! Peak velocity of jet
 
   if (first_call) call write_user_log(param_file)
   
@@ -61,24 +134,24 @@ subroutine jet_set_OBC_data(OBC, G, GV, US, PF)
   isd = G%isd ; ied = G%ied ; jsd = G%jsd ; jed = G%jed
   IsdB = G%IsdB ; IedB = G%IedB ; JsdB = G%JsdB ; JedB = G%JedB
 
-  if (.not.associated(OBC)) call MOM_error(FATAL, 'USER_initialization.F90: '// &
-        'USER_set_OBC_data() was called but OBC type was not initialized!')
+  if (.not.associated(OBC)) call MOM_error(FATAL, 'jet_initialization.F90: '// &
+        'jet_initialization() was called but OBC type was not initialized!')
   
-  km_to_L_scale = 1000.0*US%m_to_L
-  ! Pull in the Coriolis parameter
-  call get_param(PF, mdl, "F_0", F_0, &
-                 default=0.0, units="s-1", scale=US%T_to_s, do_not_log=.true.)
-   
-  ! Default parameters for the Rossby-Zhang jet
-  L     = 31.0
-  delta = 0.3
-  Ro    = 0.6
-  F     = 1.0989
-  
-  ! Unravel the dimensionless parameters into dimensional parameters
-  Ls = L*(1.0 + delta)
-  Ln = L*(1.0 - delta)
+  if (OBC%number_of_segments /= 2) then
+    call MOM_error(WARNING, 'Error in jet OBC segment setup', .true.)
+    return   !!! Need a better error message here
+  endif
 
+  
+  do n=1,OBC%number_of_segments
+    segment => OBC%segment(n)
+    ! Don't bother to do anything unless there are OB points in this tile
+    if (.not. segment%on_pe) cycle
+    
+    ! Apply values on inflow end only
+    if (segment%direction /= OBC_DIRECTION_W) cycle
+  enddo
+  
 end subroutine USER_set_OBC_data
 
 !> Write output about the parameter values being used.
